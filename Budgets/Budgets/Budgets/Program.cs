a7b36f7d070dc9a8701a052057117eba17925154
appleVovan <yablonskyi.v.reg@gmail.com>
2021-01-30 21:26:47 +0200

    Elements were added and sum calculated

--- Budgets/Budgets/Budgets/Program.cs
+++ Budgets/Budgets/Budgets/Program.cs
@@ -9,6 +9,12 @@ namespace AV.ProgrammingWithCSharp.Budgets
         {
             var transactions = new List<double>();
             transactions.Add(27.3);
+            transactions.Add(4.8);
+            transactions.Add(384.486);
+            var result = transactions[0];
+            result = result + transactions[1];
+            result = result + transactions[2];
+            Console.WriteLine(result);
         }
     }
 }
