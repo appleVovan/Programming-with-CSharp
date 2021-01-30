e47015cc95437e4c3c982abbe85f4126fffbe4c8
appleVovan <yablonskyi.v.reg@gmail.com>
2021-01-30 21:04:19 +0200

    Budgets project was added

---
+++ Budgets/Budgets/Budgets/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AV.ProgrammingWithCSharp.Budgets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
