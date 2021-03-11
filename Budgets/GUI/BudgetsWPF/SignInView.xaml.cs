87fa777de855488352486ea54c97c8e91aea0510
appleVovan <yablonskyi.v.reg@gmail.com>
2021-03-11 17:51:45 +0200

    WPF for budgets (Login view)

---
+++ Budgets/GUI/BudgetsWPF/SignInView.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AV.ProgrammingWithCSharp.Budgets.GUI.WPF
+{
+    /// <summary>
+    /// Interaction logic for SignInView.xaml
+    /// </summary>
+    public partial class SignInView : UserControl
+    {
+        public SignInView()
+        {
+            InitializeComponent();
+            BSignIn.IsEnabled = false;
+        }
+
+        private void BClose_OnClick(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void BSignIn_Click(object sender, RoutedEventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(TbLogin.Text) || String.IsNullOrWhiteSpace(TbPassword.Text))
+                MessageBox.Show("Login or password is empty.");
+            else
+            {
+                var authUser = new AuthenticationUser()
+                {
+                    Login = TbLogin.Text,
+                    Password = TbPassword.Text
+                };
+                var authService = new AuthenticationService();
+                User user = null;
+                try
+                {
+                    user = authService.Authenticate(authUser);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Sign In failed: {ex.Message}");
+                    return;
+                }
+                MessageBox.Show($"Sign In was successful for user {user.FirstName} {user.LastName}");
+                //TODO Navigate to Main View of Application
+            }
+        }
+
+        private void Input_Changed(object sender, TextChangedEventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(TbLogin.Text) || String.IsNullOrWhiteSpace(TbPassword.Text))
+                BSignIn.IsEnabled = false;
+            else
+                BSignIn.IsEnabled = true;
+        }
+    }
+}
